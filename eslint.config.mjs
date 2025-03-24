766774489abff96fc046e1cdce8f2cbc54ee151f
smhex <smhex@mailbox.org>
2025-03-24 23:58:47 +0100

    Removed prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,12 @@
-import eslintRecommended from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
-import prettierConfig from 'eslint-config-prettier';
-import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default [
-  // Allgemeine Konfiguration für alle Dateien
-  {
-    plugins: {
-      '@typescript-eslint': tseslint,
-      prettier: prettierPlugin,
-    },
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      ...eslintRecommended.configs.recommended.rules, // ESLint empfohlene Regeln
-      ...tseslint.configs.recommended.rules, // TypeScript empfohlene Regeln
-      'prettier/prettier': ['error', { singleQuote: true }], // Prettier-Formatierung als Fehler
-    },
-  },
 
-  // Spezifische Konfiguration für TypeScript-Dateien
-  {
-    files: ['src/**/*.ts'],
-    rules: {
-      'max-len': ['error', { code: 80 }], // Maximale Zeilenlänge für TypeScript
-      'max-lines': ['error', { max: 1000 }], // Maximale Anzahl an Zeilen
-    },
-  },
-
-  // Konfliktlösungen für Prettier
-  prettierConfig,
-];
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  tseslint.configs.recommended,
+]);
