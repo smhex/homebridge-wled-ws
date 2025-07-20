57d6258745a5c18843dd77c4958d908eee6049e1
smhex <smhex@mailbox.org>
2025-07-20 17:39:05 +0200

    Added jest and first test cases

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { API } from 'homebridge';
 
 import { PLATFORM_NAME } from './settings';
-import { WledWsHomebridgePlatform } from './WledWsPlatform';
+import { WledWsHomebridgePlatform } from './WledWsHomebridgePlatform';
 
 /**
  * This method registers the platform with Homebridge
