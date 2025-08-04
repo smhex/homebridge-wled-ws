93c2a6991b5cf0a6306f10151652b4588f1b7b96
smhex <smhex@mailbox.org>
2025-08-04 18:44:43 +0200

    Reduced log messages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,22 @@ import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  { ignores: ["dist", "build", "test", "node_modules"],},
-  { files: ["src/**/*.{ts}"] },
+  {
+    files: ["src/**/*.ts"],
+    ignores: ["dist", "build", "test", "node_modules"],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      // Indent with 2 spaces
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'no-tabs': 'error',
+      'no-mixed-spaces-and-tabs': 'error',
+
+      // Use single quotes for strings
+      quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],      
+      semi: ['error', 'always'],
+    },
+  },
   tseslint.configs.recommended,
 ]);
